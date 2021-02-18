6e2fd11506c947a7bf0161b7e2c3573a86abb351
Patrick Mischler <patrick_mischler@icloud.com>
2021-02-18 12:15:25 +0100

    Enhanced logging and error handling

--- promalert2bmctsom.go
+++ promalert2bmctsom.go
@@ -185,8 +185,10 @@ func (config Config) Run() {
 			}
 
 			var alerts Alerts
-			json.Unmarshal(body, &alerts)
-			events := []*TSEvent{}
+			if err := json.Unmarshal(body, &alerts); err != nil {
+				log.Println("JSON unmarshaling has errors: ", err)
+			}
+			var events []*TSEvent
 			for alert := range alerts.Alerts {
 				//fmt.Println(alerts.Alerts[alert].Status)
 				if alerts.Alerts[alert].Status == "firing" {
@@ -219,22 +221,10 @@ func (config Config) Run() {
 	log.Fatal(http.ListenAndServe(":"+config.Server.ListenPort, nil))
 }
 
-func main() {
-	cfgPath, err := ParseFlags()
-	if err != nil {
-		log.Fatal(err)
-	}
-	cfg, err := NewConfig(cfgPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	cfg.Run()
-}
-
 func GetTSToken(tspsServer string, tspsPort string, tsUser string, tsUserPw string, tsTenant string) string {
 	authToken := "undefined"
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	tokenUrl := "https://" + tspsServer + ":" + string(tspsPort) + "/tsws/api/v10.1/token"
+	tokenUrl := "https://" + tspsServer + ":" + tspsPort + "/tsws/api/v10.1/token"
 	postData := map[string]string{"username": tsUser, "password": tsUserPw, "tenantName": tsTenant}
 	postDataJson, err := json.Marshal(postData)
 
@@ -242,21 +232,35 @@ func GetTSToken(tspsServer string, tspsPort string, tsUser string, tsUserPw stri
 		log.Fatal(err)
 	}
 
-	resp, err := http.Post(tokenUrl, "application/json",
-		bytes.NewBuffer(postDataJson))
+	//retries := 5
+	retryCount := 0
 
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		defer resp.Body.Close()
-		rawData, _ := ioutil.ReadAll(resp.Body)
-		var data map[string]map[string]interface{}
-		json.Unmarshal(rawData, &data)
-		/*if err != nil {
-			log.Fatal(err)
-		}*/
-		authToken = data["response"]["authToken"].(string)
-		//fmt.Println(authToken)
+	//for retryCount < retries {
+	for {
+		resp, err := http.Post(tokenUrl, "application/json", bytes.NewBuffer(postDataJson))
+		if err != nil {
+			log.Println(err)
+			//if retryCount == 4 {
+			//	os.Exit(3)
+			//} else {
+			time.Sleep(10 * time.Second)
+			retryCount++
+			//}
+
+		} else {
+			//resp.Body.Close()
+			rawData, _ := ioutil.ReadAll(resp.Body)
+			log.Println("Response: ", rawData)
+			var data map[string]map[string]interface{}
+			json.Unmarshal(rawData, &data)
+			/*if err := json.Unmarshal(rawData, &data); err != nil {
+				log.Println("token data received")
+				log.Fatal(err)
+			}*/
+			authToken = data["response"]["authToken"].(string)
+			break
+			//fmt.Println(authToken)
+		}
 	}
 	//fmt.Println("Auth Token is: " + authToken)
 	return authToken
@@ -264,7 +268,7 @@ func GetTSToken(tspsServer string, tspsPort string, tsUser string, tsUserPw stri
 
 func VerifyTSToken(token string, tspsServer string, tspsPort string) bool {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	tokenUrl := "https://" + tspsServer + ":" + string(tspsPort) + "/tsws/api/v10.1/token"
+	tokenUrl := "https://" + tspsServer + ":" + tspsPort + "/tsws/api/v10.1/token"
 	//fmt.Println("Token: " + token)
 	req, err := http.NewRequest("GET", tokenUrl, nil)
 	if err != nil {
@@ -284,7 +288,9 @@ func VerifyTSToken(token string, tspsServer string, tspsPort string) bool {
 		defer resp.Body.Close()
 		rawData, _ := ioutil.ReadAll(resp.Body)
 		var data map[string]interface{}
-		json.Unmarshal(rawData, &data)
+		if err := json.Unmarshal(rawData, &data); err != nil {
+			log.Fatal(err)
+		}
 		//statusCode := data["statusCode"].(string)
 		statusMsg := data["statusMsg"].(string)
 		//fmt.Println("Status Code: " + statusCode)
@@ -320,8 +326,28 @@ func SendEventToTS(token string, tsimServer string, tsimPort string, tsCell stri
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	fmt.Println(resp)
 	return true
+}
+
+func main() {
+	// Initialize logging
+	logFileName := "promalert2bmctsom.log"
+	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal("Unable to create logfile: ", err)
+	}
+	log.SetOutput(logFile)
+	log.Println("Prometheus Alert to BMC TSOM Wrapper started")
+	cfgPath, err := ParseFlags()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg, err := NewConfig(cfgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg.Run()
 }
